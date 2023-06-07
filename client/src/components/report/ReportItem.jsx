2406bd7ca8151abfa298e8f8e57b1adb15f859d0
aydenote <dslgpgh@gmail.com>
2023-06-07 17:05:30 +0900

    게시글 컴포넌트에 게시글 생성, 수정, 삭제 컴포넌트 연결

--- client/src/components/report/ReportItem.jsx
+++ client/src/components/report/ReportItem.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import ThumbnailTitle from './ThumbnailTitle';
 import ThumbnailContent from './ThumbnailContent';
+import AddBtn from '../Button/Add';
+import ModifyBtn from '../Button/Modify';
+import DeleteBtn from '../Button/Delete';
 import { styled } from 'styled-components';
 
 const ReportItem = () => {
+  const pathName = useLocation().pathname;
   return (
     <Container>
       <ThumbnailTitle>더 리더</ThumbnailTitle>
+      {pathName !== '/' ? (
+        <BtnWrap>
+          <AddBtn />
+          <ModifyBtn />
+          <DeleteBtn />
+        </BtnWrap>
+      ) : null}
       <Date>06.06.2023</Date>
       <ThumbnailContent />
     </Container>
@@ -21,6 +33,12 @@ const Container = styled.section`
   flex-shrink: 0;
   min-height: 100%;
   scroll-snap-align: start;
+`;
+
+const BtnWrap = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  gap: 20px;
 `;
 
 const Date = styled.p`
