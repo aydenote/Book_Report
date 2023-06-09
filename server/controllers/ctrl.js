7cffb20ac167dce49f013d4814ad4d23c671a323
aydenote <dslgpgh@gmail.com>
2023-06-09 22:24:08 +0900

    사용자 회원가입 컨트롤러 구현

--- server/controllers/ctrl.js
+++ server/controllers/ctrl.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const User = require('../model/User');
 
 const output = (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
@@ -9,7 +10,15 @@ const process = {
     console.log('로그인');
   },
   register: async (req, res) => {
-    console.log(req.body.data);
+    const user = new User(req.body.data);
+    const response = await user.register();
+    const url = {
+      method: 'POST',
+      path: '/register',
+      status: response.err ? 409 : 201,
+    };
+
+    return res.status(url.status).json(response);
   },
 };
 
