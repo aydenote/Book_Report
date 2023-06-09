47c0628d10a2dc7e4bfbfc27485c1b67adcf4faf
aydenote <dslgpgh@gmail.com>
2023-06-09 22:23:44 +0900

    사용자 회원가입 모델 구현

---
+++ server/model/User.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const UserStorage = require('./UserStorage');
+
+class User {
+  constructor(body) {
+    this.body = body;
+  }
+
+  async register() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+}
+
+module.exports = User;
