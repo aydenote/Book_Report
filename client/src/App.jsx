410ad9e9d5a2179ee9f5832cb5dd0a71998859fc
aydenote <dslgpgh@gmail.com>
2023-06-15 01:19:03 +0900

    게시물, 일기 생성 페이지 경로 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Report from './pages/Report';
 import Diary from './pages/Diary';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Write from './pages/Write';
 import GlobalStyle from './GlobalStyles';
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
           <Route path="/diary" element={<Diary />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/report/write" element={<Write />} />
+          <Route path="/diary/write" element={<Write />} />
         </Routes>
       </BrowserRouter>
     </>
