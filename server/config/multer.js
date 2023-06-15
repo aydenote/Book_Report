c091d71cb89987a7afd141d14948b3d2f530cce3
aydenote <dslgpgh@gmail.com>
2023-06-15 12:40:57 +0900

    업로드 이미지 파일 크기 제한 설정

--- server/config/multer.js
+++ server/config/multer.js
@@ -3,12 +3,13 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'uploads');
   },
   filename(req, file, cb) {
     const ext = path.extname(file.originalname);
     cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
   },
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 const upload = multer({ storage });
