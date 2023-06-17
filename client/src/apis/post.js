4f5ab425e11347f40c823bf864838c97fedc284f
aydenote <dslgpgh@gmail.com>
2023-06-17 16:36:20 +0900

    게시물 생성 API 함수에 쿠키 적용

--- client/src/apis/post.js
+++ client/src/apis/post.js
@@ -1,6 +1,11 @@
 import aixos from 'axios';
+import { getCookie } from '../cookie';
 
 export const createPost = async formData => {
-  const res = await aixos.post('http://localhost:8080/report/write', formData);
+  const res = await aixos.post('http://localhost:8080/report/write', formData, {
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+    },
+  });
   return res.data;
 };
