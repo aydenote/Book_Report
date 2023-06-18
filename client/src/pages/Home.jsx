a5ff7be40d5eb4fbb79870bda0c961f2ded7bb5e
aydenote <dslgpgh@gmail.com>
2023-06-18 14:53:57 +0900

    홈 페이지에 비로그인 레이아웃 컴포넌트 연결

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import MenuLayout from '../layout/Menu.layout';
-import HomeLayout from '../layout/Home.layout';
+import PostLayout from '../layout/Post.layout';
 import ReportPinLayout from '../layout/ReportPin.layout';
 import ReportThumbnailLayout from '../layout/ReportThumbnail.layout';
 import DiaryLayout from '../layout/Diary.layout';
+import LoginLinkLayout from '../layout/LoginLink.layout';
+import { getCookie } from '../cookie';
 
 const Home = () => {
+  const token = getCookie('token');
+
   return (
-    <HomeLayout>
+    <PostLayout>
       <MenuLayout />
-      <ReportPinLayout />
-      <ReportThumbnailLayout />
-      <DiaryLayout />
-    </HomeLayout>
+      {token ? (
+        <>
+          <ReportPinLayout />
+          <ReportThumbnailLayout />
+          <DiaryLayout />
+        </>
+      ) : (
+        <LoginLinkLayout />
+      )}
+    </PostLayout>
   );
 };
 
