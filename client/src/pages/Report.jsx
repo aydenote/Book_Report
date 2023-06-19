5c8e8217de0d08740f675dd1c43c6f88da867f3e
aydenote <dslgpgh@gmail.com>
2023-06-19 18:41:39 +0900

    토큰이 없는 경우 독서 감상문 페이지 로그인 및 회원가입 링크 컴포넌트 연결

--- client/src/pages/Report.jsx
+++ client/src/pages/Report.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReportLayout from '../layout/Post.layout';
 import MenuLayout from '../layout/Menu.layout';
 import ReportThumbnail from '../layout/ReportThumbnail.layout';
+import LoginLinkLayout from '../layout/LoginLink.layout';
+import { getCookie } from '../cookie';
 
 const Report = () => {
+  const token = getCookie('token');
   return (
     <ReportLayout>
       <MenuLayout />
-      <ReportThumbnail />
+      {token ? <ReportThumbnail /> : <LoginLinkLayout />}
     </ReportLayout>
   );
 };
