003cafb571c926dfe5a17d6efdd5e3328291f122
aydenote <dslgpgh@gmail.com>
2023-06-20 21:08:35 +0900

    게시물 정보 요청 함수 구현

--- client/src/apis/post.js
+++ client/src/apis/post.js
@@ -9,3 +9,15 @@ export const createPost = async formData => {
   });
   return res.data;
 };
+
+export const getPost = async id => {
+  const res = await aixos.get(`http://localhost:8080/report/read`, {
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+    },
+    params: {
+      id,
+    },
+  });
+  return res.data;
+};
