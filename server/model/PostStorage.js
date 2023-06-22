01d6883463c38c08bc2a5db46830549527fb5075
aydenote <dslgpgh@gmail.com>
2023-06-22 10:39:46 +0900

    mysql 사용자 게시물 정보 요청 구현

--- server/model/PostStorage.js
+++ server/model/PostStorage.js
@@ -1,6 +1,16 @@
 const db = require('../config/db');
 
 class PostStorage {
+  static getPostsInfo(id) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM posts WHERE userId = ?;';
+      db.query(query, [id], (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data);
+      });
+    });
+  }
+
   static async save(userId, postId, title, content, imagePath) {
     return new Promise((resolve, reject) => {
       const query = 'INSERT INTO posts(userId, postId, title, content, imagePath) VALUES(?, ?, ?, ?, ?);';
