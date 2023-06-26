f3fd2fdd56bfb31bdeda8c2de7fc2ef3465c900a
aydenote <dslgpgh@gmail.com>
2023-06-27 00:40:46 +0900

    게시물 삭제 라우터 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,8 @@ router.post('/login', controller.process.login);
 router.post('/register', controller.process.register);
 
 // 게시물 정보
-router.post('/report/write', authenticateToken, upload.single('image'), controller.process.create);
 router.get('/report/read', authenticateToken, controller.process.read);
+router.post('/report/write', authenticateToken, upload.single('image'), controller.process.create);
+router.delete('/report/delete', authenticateToken, controller.process.delete);
 
 module.exports = router;
