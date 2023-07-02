5b22d24aaff7ff69722ff2f6d97f5a6e2454a753
aydenote <dslgpgh@gmail.com>
2023-07-02 20:57:02 +0900

    db 서버에 저장된 게시물 정보를 redux state에 데이터 fetch 구현

--- client/src/layout/ReportThumbnail.layout.jsx
+++ client/src/layout/ReportThumbnail.layout.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPost } from '../redux/action';
 import ReportItem from '../components/report/ReportItem';
+import Add from '../components/button/Add';
+import { getPost } from '../apis/post';
 import { styled } from 'styled-components';
 
 const ReportThumbnail = () => {
+  const dispatch = useDispatch();
+  const postsInfo = useSelector(state => state.postInfo.data);
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      try {
+        const response = await getPost();
+        dispatch(setPost(response));
+      } catch (error) {
+        const errData = await error.response.data;
+        alert(errData.msg);
+      }
+    };
+    fetchPostData();
+  }, [dispatch]);
+
   return (
-    <Container>
-      <ReportItem />
-      <ReportItem />
-      <ReportItem />
-    </Container>
+    <>
+      {postsInfo.length > 0 ? (
+        <PostContainer>
+          {postsInfo.map(post => (
+            <ReportItem post={post} key={post.postId} />
+          ))}
+        </PostContainer>
+      ) : (
+        <PostAddContainer>
+          <Notice>게시물이 없습니다.</Notice>
+          <Add />
+        </PostAddContainer>
+      )}
+    </>
   );
 };
 
 export default ReportThumbnail;
 
-const Container = styled.section`
+const PostContainer = styled.section`
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -30,4 +59,20 @@ const Container = styled.section`
   &::-webkit-scrollbar {
     display: none; /* Chrome, Safari, Opera*/
   }
+`;
+
+const PostAddContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 20px;
+  border-right: 1px solid #94918f;
+`;
+
+const Notice = styled.p`
+  margin: 10px 0;
+  text-align: center;
+  font-size: 2vw;
+  font-weight: 800;
 `;
