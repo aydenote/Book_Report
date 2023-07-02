a6815ca960345abbb665f228e77e1b8c6cedc551
aydenote <dslgpgh@gmail.com>
2023-07-02 20:53:15 +0900

    refresh 토큰을 이용한 access 토큰 재발급 API 함수 구현

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -1,4 +1,5 @@
 import aixos from 'axios';
+import { getCookie } from '../cookie';
 
 export const handleLogin = async (id, password) => {
   const res = await aixos.post('http://localhost:8080/login', {
@@ -18,5 +19,20 @@ export const handleSignup = async (name, id, password) => {
       password,
     },
   });
+  return res.data;
+};
+
+export const handleRenewToken = async () => {
+  const userId = getCookie('id');
+  const refreshToken = getCookie('refreshToken');
+  const res = await aixos.post(
+    'http://localhost:8080/refresh',
+    { params: { userId } },
+    {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+      },
+    }
+  );
   return res.data;
 };
