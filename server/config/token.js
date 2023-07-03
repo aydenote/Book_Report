4c643708d52fdf348bd2daa9557b28712f9d9096
aydenote <dslgpgh@gmail.com>
2023-07-03 16:51:55 +0900

    refresh 토큰 인증 미들웨어 구현

--- server/config/token.js
+++ server/config/token.js
@@ -55,8 +55,39 @@ const verifyAccessToken = (req, res, next) => {
   });
 };
 
+/** Refresh Token 검증 함수 */
+const verifyRefreshToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const clientRefreshToken = authHeader && authHeader.split('Bearer ')[1];
+  const userId = req.body.params.userId;
+  const dbUserInfo = await UserStorage.getUserInfo(userId);
+
+  if (!clientRefreshToken || !dbUserInfo.refreshToken) {
+    res.status(401).send({ type: 'lost', msg: 'Refresh 인증 토큰이 없습니다.' });
+  }
+
+  if (dbUserInfo.refreshToken === clientRefreshToken) {
+    jwt.verify(clientRefreshToken, process.env.REFRESH_TOKEN_KEY, err => {
+      if (err) {
+        if (err.name === 'TokenExpiredError') {
+          res.status(401).send({ type: 'expired', msg: 'Refresh 인증 토큰이 만료되었습니다.' });
+        } else if (err.name === 'JsonWebTokenError') {
+          res.status(401).send({ type: 'damaged', msg: 'Refresh 인증 토큰이 손상되었습니다.' });
+        } else {
+          res.status(401).send({ type: 'damaged', msg: '유효하지 않은 Refresh 인증 토큰입니다.' });
+        }
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ type: 'match', msg: 'Refresh 인증 토큰 정보가 일치하지 않습니다.' });
+  }
+};
+
 module.exports = {
   createAccessToken,
   createRefreshToken,
   verifyAccessToken,
+  verifyRefreshToken,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const controller = require('../controllers/ctrl');
 const upload = require('../config/multer');
-const { verifyAccessToken } = require('../config/token');
+const { verifyAccessToken, verifyRefreshToken } = require('../config/token');
 const router = express.Router();
 
 // 사용자 정보
 router.post('/login', controller.process.login);
 router.post('/register', controller.process.register);
+router.post('/refresh', verifyRefreshToken, controller.process.createToken);
 
 // 게시물 정보
 router.get('/report/read', verifyAccessToken, controller.process.read);
