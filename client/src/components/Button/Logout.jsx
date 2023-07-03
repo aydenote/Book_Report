26e974810c9f967456f18cff1b40843958d2c85d
aydenote <dslgpgh@gmail.com>
2023-07-03 17:13:40 +0900

    로그아웃 함수 import 오타 수정

--- client/src/components/Button/Logout.jsx
+++ client/src/components/Button/Logout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import handleLogout from '../../util/logout';
+import { handleLogout } from '../../util/logout';
 import { styled } from 'styled-components';
 
 const Logout = () => {
