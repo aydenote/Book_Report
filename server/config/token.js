0a8ef948aee093c7e541f439e06c2537d1c394c9
aydenote <dslgpgh@gmail.com>
2023-07-03 16:51:13 +0900

    access 토큰 검증 미들웨어 구현

--- server/config/token.js
+++ server/config/token.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const UserStorage = require('../model/UserStorage');
 
-// JWT 토큰 설정
+/** JWT Access 토큰 설정 */
 const createAccessToken = userId => {
   return jwt.sign(
     {
@@ -15,6 +16,7 @@ const createAccessToken = userId => {
   );
 };
 
+/** JWT Refresh 토큰 설정 */
 const createRefreshToken = userId => {
   return jwt.sign(
     {
@@ -29,13 +31,24 @@ const createRefreshToken = userId => {
   );
 };
 
-// 인증 미들웨어
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split('Bearer ')[1];
-  jwt.verify(token, 'secret_blog', err => {
+/** Access Token 검증 함수 */
+const verifyAccessToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const accessToken = authHeader && authHeader.split('Bearer ')[1];
+
+  if (!accessToken) {
+    res.status(401).send({ type: 'lost', msg: 'Access 인증 토큰이 없습니다.' });
+  }
+
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN_KEY, err => {
     if (err) {
-      res.status(401).json({ error: '인증 문제로 게시물 생성에 실패하였습니다.' });
+      if (err.name === 'TokenExpiredError') {
+        res.status(401).send({ type: 'expired', msg: 'Access 인증 토큰이 만료되었습니다.' });
+      } else if (err.name === 'JsonWebTokenError') {
+        res.status(401).send({ type: 'damaged', msg: 'Access 인증 토큰이 손상되었습니다.' });
+      } else {
+        res.status(401).send({ type: 'damaged', msg: '유효하지 않은 Access 인증 토큰입니다.' });
+      }
     } else {
       next();
     }
@@ -45,5 +58,5 @@ const authenticateToken = (req, res, next) => {
 module.exports = {
   createAccessToken,
   createRefreshToken,
-  authenticateToken,
+  verifyAccessToken,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const controller = require('../controllers/ctrl');
 const upload = require('../config/multer');
-const { authenticateToken } = require('../config/token');
+const { verifyAccessToken } = require('../config/token');
 const router = express.Router();
 
 // 사용자 정보
@@ -11,8 +11,8 @@ router.post('/login', controller.process.login);
 router.post('/register', controller.process.register);
 
 // 게시물 정보
-router.get('/report/read', authenticateToken, controller.process.read);
-router.post('/report/write', authenticateToken, upload.single('image'), controller.process.create);
-router.delete('/report/delete', authenticateToken, controller.process.delete);
+router.get('/report/read', verifyAccessToken, controller.process.read);
+router.post('/report/write', verifyAccessToken, upload.single('image'), controller.process.create);
+router.delete('/report/delete', verifyAccessToken, controller.process.delete);
 
 module.exports = router;
