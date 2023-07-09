cd18d144752f24a4d1d2021c3eec5e40980b5174
aydenote <dslgpgh@gmail.com>
2023-07-09 19:53:35 +0900

    게시물 아이디를 이용한 게시물 정보 요청 라우터 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,8 @@ router.post('/register', controller.process.register);
 router.post('/refresh', verifyRefreshToken, controller.process.createToken);
 
 // 게시물 정보
-router.get('/report/read', verifyAccessToken, controller.process.read);
+router.get('/report/readAllPost', verifyAccessToken, controller.process.readAllPost);
+router.get('/report/readSinglePost', verifyAccessToken, controller.process.readSinglePost);
 router.post('/report/write', verifyAccessToken, upload.single('image'), controller.process.create);
 router.delete('/report/delete', verifyAccessToken, controller.process.delete);
 router.get('/image/uploads/:imagePath', controller.process.image);
