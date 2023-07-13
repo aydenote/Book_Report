ab11ee78a55221bfdf13b888461793d6def8ae22
aydenote <dslgpgh@gmail.com>
2023-07-13 20:49:00 +0900

    일기 모델 구현 및 일기 생성 기능 추가

---
+++ server/model/Diary.js
@@ -0,0 +1,25 @@
+const DiaryStorage = require('./DiaryStorage');
+
+class Diary {
+  constructor(body) {
+    this.body = body;
+  }
+
+  async addDiary() {
+    const client = this.body;
+    try {
+      const response = await DiaryStorage.save(
+        client.userId,
+        client.diaryId,
+        client.diaryTitle,
+        client.content,
+        client.date
+      );
+      return response;
+    } catch (err) {
+      return { success: false, err };
+    }
+  }
+}
+
+module.exports = Diary;
