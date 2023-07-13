488db92fcaaf768ce00e3cc5ab5862c3ae7be97e
aydenote <dslgpgh@gmail.com>
2023-07-14 00:19:30 +0900

    db에서 일기 정보를 가져오는 기능 추가

--- server/model/DiaryStorage.js
+++ server/model/DiaryStorage.js
@@ -1,6 +1,16 @@
 const db = require('../config/db');
 
 class DiaryStorage {
+  static getAllDiaryInfo(userId) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM diary WHERE userId = ?;';
+      db.query(query, [userId], (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data);
+      });
+    });
+  }
+
   static async save(userId, diaryId, diaryTitle, content, date) {
     return new Promise((resolve, reject) => {
       const query = 'INSERT INTO diary(userId, diaryId, diaryTitle, content, date) VALUES(?, ?, ?, ?, ?);';
