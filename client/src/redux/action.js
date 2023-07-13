aea404a4040334d12356abb9fae6d6613d6f389e
aydenote <dslgpgh@gmail.com>
2023-07-14 00:20:36 +0900

    일기 데이터 fetch 액션 함수 추가

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,6 +1,12 @@
 export const SET_POST = 'SET_POST';
+export const SET_DIARY = 'SET_DIARY';
 
 export const setPost = posts => ({
   type: SET_POST,
   payload: posts,
+});
+
+export const setDiary = diary => ({
+  type: SET_DIARY,
+  payload: diary,
 });
