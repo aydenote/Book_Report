bd34152dba87a9c661300d26d0c1cdc74733c603
aydenote <dslgpgh@gmail.com>
2023-07-14 00:22:40 +0900

    루트 리듀서에 일기 리듀서 추가

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import postInfo from './post.reducer';
+import diaryInfo from './diary.reducer';
 
 const rootReducer = combineReducers({
   postInfo,
+  diaryInfo,
 });
 
 export default rootReducer;
