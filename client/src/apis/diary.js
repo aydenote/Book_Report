f9c60c182f5824b76ee75b7db76dfe60a0e47c92
aydenote <dslgpgh@gmail.com>
2023-07-14 00:16:37 +0900

    일기 전체 정보를 가져오는 API 함수 추가

--- client/src/apis/diary.js
+++ client/src/apis/diary.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 import { getCookie } from '../cookie';
 
+export const getAllDiary = async () => {
+  const userId = getCookie('id');
+  const accessToken = getCookie('accessToken');
+  const res = await axios.get(`http://localhost:8080/diary/readAllDiary`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      userId,
+    },
+  });
+  return res.data;
+};
+
 export const createDiary = async formData => {
   const accessToken = getCookie('accessToken');
   const res = await axios.post('http://localhost:8080/diary/write', formData, {
